d6b3fe1d5417bcad29050dda859f50f12407265b
Igor Gadomskii <igvgad@gmail.com>
2022-03-10 18:15:59 +0300

    Created 4 elements and added to view controller. Added simple logic.

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    
+    @IBOutlet weak var changeLightButton: UIButton!
+    
+    enum ChangeColor {
+        case red
+        case yellow
+        case green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColor.alpha = 0.3
+        redColor.layer.cornerRadius = 50
+        
+        yellowColor.alpha = 0.3
+        yellowColor.layer.cornerRadius = 50
+        
+        greenColor.alpha = 0.3
+        greenColor.layer.cornerRadius = 50
+        
     }
 
-
+    @IBAction func changeTrafficLight() {
+        
+        if redColor.alpha == 1.0 {
+            yellowColor.alpha = 1.0
+            redColor.alpha = 0.3
+            changeLightButton.setTitle("Next", for: .normal)
+        } else if yellowColor.alpha == 1.0 {
+            greenColor.alpha = 1.0
+            yellowColor.alpha = 0.3
+            changeLightButton.setTitle("Go!", for: .normal)
+        } else {
+            greenColor.alpha = 0.3
+            redColor.alpha = 1.0
+            changeLightButton.setTitle("Next", for: .normal)
+        }
+    }
 }
 
